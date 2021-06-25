451c7a48c2ff5dba84edcd4e86fbabf8fb5f90c3
suryaravikumar-pixer <suryaravikumar@gmail.com>
2021-06-25 10:29:45 +0530

    Union operation is performed

--- student/models.py
+++ student/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Teacher(models.Model):
     firstname = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
+    salary = models.IntegerField(null=True)
 
     def __str__(self):
         return self.firstname
@@ -13,7 +14,7 @@ class Student(models.Model):
     surname = models.CharField(max_length=100)
     age = models.IntegerField()
     classroom = models.IntegerField()
-    teacher = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.firstname
--- student/views.py
+++ student/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render
-from .models import Student
+from .models import Student, Teacher
 from django.db import connection #provides additional info in query like LIMIT, x - time 0.0015sec
 from django.db.models import Q 
 """
@@ -9,9 +9,13 @@ Q   objects are helpfull for complex queries because they can be combined using
     logical operators and(&), or(|), negation(~)
 """
 
+"""Union operation returns the all the records after eleminating the duplicates"""
 def student_list(request):
-    posts = Student.objects.filter(Q(surname__startswith='E') & Q(firstname__startswith='N')) 
+    posts = Student.objects.all().values_list("firstname").union(
+        Teacher.objects.all().values_list("firstname"))
+    
     print(posts)
+    print(connection.queries)
     return render(request, 'output.html', {'posts':posts})
 
 
